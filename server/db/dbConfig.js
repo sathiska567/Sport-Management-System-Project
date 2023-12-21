bbf6fc09e65d5c7708c6508f613cf38dd5f6885c
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2023-12-21 17:31:21 +0530

    added

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const mongoURL = "mongodb+srv://aathik:789@cluster0.w2q6z29.mongodb.net/SMS?retryWrites=true&w=majority";
+const mongoURL = "mongodb+srv://sasindusathiska:sportManagementSystem@cluster0.oph32oc.mongodb.net/SMS?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
--- server/index.js
+++ server/index.js
@@ -30,16 +30,6 @@ initDatabase();
 app.use('/admin', adminRoutes);
 
 
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
