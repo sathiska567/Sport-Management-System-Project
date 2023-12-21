30b3b12222ab5dcbb84e5bc417a464062a453b51
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2023-12-21 16:43:20 +0530

    changed

--- project/src/App.js
+++ project/src/App.js
@@ -3,43 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserApplication from "./Components/UserApplication/UserApplication.jsx";
 import UserApplicationTable from "./Components/UserApplicationTable/UserApplicationTable.jsx";
 import StatCards from "./Components/StatCard/StatCards.jsx";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute.js";
-import PublicRoute from "./Components/PublicRoute/PublicRoute.js";
-import Login from "./Pages/LoginPage.jsx";
-import Register from "./Pages/RegisterPage.jsx";
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <ProtectedRoute>
+          
                 <StatCards />
-          </ProtectedRoute>
+          
             
         
         } />
 
-       {/* All user Login */}
-       <Route path="/login" element={
-              <PublicRoute>
-                    <Login />
-              </PublicRoute>
-               
-             
-          } />
-          
-          {/* All user registration */}
-          <Route path="/register" element={
-             
-               <PublicRoute>
-                 <Register />
-               </PublicRoute>
-            
-          } /> 
-
-
-
+      
         <Route
           path="/UserValidation"
           element={<UserApplicationTable />}
