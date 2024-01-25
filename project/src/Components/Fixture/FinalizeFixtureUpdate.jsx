296f3277b660549ee7af9cca0f5c746944f396cd
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-01-25 21:19:52 +0530

    updated

---
+++ project/src/Components/Fixture/FinalizeFixtureUpdate.jsx
@@ -0,0 +1,130 @@
+// import React, { useEffect, useState } from 'react'
+// import SideBar from '../DashboardSideBar/SideBar'
+// import axios from 'axios'
+// import { Input, Table, message } from 'antd'
+// import { useNavigate } from 'react-router-dom'
+
+// export default function FinalizeFixtureUpdate() {
+//   const [newTeamdata, setnewTeamData] = useState([])
+//   const [fixtures, setFixture] = useState([])
+//   const navigate = useNavigate();
+
+// const getAllFixtures = async () => {
+
+//     try {
+//       const response = await axios.get("http://localhost:8080/api/v1/finalFixture/all-fixtures")
+//       console.log(response.data.allFixtures);
+//       setFixture(response.data.allFixtures)
+
+//     } catch (error) {
+//       message.error("Error in fetching data");
+//     }
+
+//   }
+
+
+// const handleDelete = async (id) => {
+//     console.log(id);
+//     try {
+//       // const id = id;
+//       const response = await axios.post("http://localhost:8080/api/v1/finalFixture/delete", { id: id })
+//       // console.log(response);
+//       message.success("Data deleted successfully");
+//       window.location.reload();
+
+//     } catch (error) {
+//       message.error("Error in deleting data");
+//     }
+//   }
+
+
+// const handleUpdate = async(id)=>{
+//     // console.log(id);
+//     // const response = await axios.post("http://localhost:8080/api/v1/finalFixture/update",{id})
+//     // console.log(response);
+//     navigate("/final-update-fixture",{state:{id:id}})
+
+//   }
+
+//   useEffect(() => {
+//     getAllFixtures()
+//   }, [])
+
+//   return (
+//     <>
+//       <SideBar>
+//         <div className="fixtureContainer">
+
+//           <Table
+//             className="Table"
+//             columns={[
+//               {
+//                 title: "Teams Name",
+//                 dataIndex: "teamName",
+//                 // width: "40%",
+//                 render: (text, record) => (
+//                   <>
+//                     <span>
+//                       {/* {record.newTeam} */}
+//                       {setnewTeamData(record.newTeam)}
+//                       {newTeamdata.map((data) => (
+//                         <ul>
+//                           <li>{data}</li>
+//                         </ul>
+
+//                       ))}
+
+//                     </span>
+
+//                   </>
+//                 )
+//               },
+
+//               {
+//                 title: "Event Time",
+//                 dataIndex: "time",
+//                 render: (text, record) => <span>8.30am</span>,
+//               },
+
+//               {
+//                 title: "Location",
+//                 dataIndex: "location",
+//                 render: (text, record) => <span>Ground 01</span>,
+//               },
+
+//               {
+//                 title: "Action",
+//                 dataIndex: "action",
+//                 render: (text, record) => (
+//                   <>
+                                   
+//                     <button onClick={()=>handleUpdate(record._id)}>Update</button>
+
+//                     <button onClick={() => handleDelete(record._id)}>Delete</button>
+
+//                   </>
+//                 )
+//               },
+
+//             ]}
+//             pagination={{
+//               style: {
+//                 marginTop: "50px",
+//               },
+//               pageSize: 100,
+//             }}
+
+//             // Displaying data from the backend
+//             dataSource={fixtures}
+//           >
+
+//           </Table>
+//         </div>
+
+//         {/* <Input placeholder="Basic usage" />; */}
+
+//       </SideBar>
+
+//     </>
+//   )
+// }
---
+++ project/src/Components/Fixture/Fixture.jsx
@@ -0,0 +1,232 @@
+// import React from 'react'
+// import SideBar from './../DashboardSideBar/SideBar';
+
+// import { useEffect, useState } from 'react'
+
+// export default function Fixture() {
+
+// //         const myArray = ["kamal","piyal","sunil","rahal"]
+// //         const shuffledArray = []
+// //         const usedIndexes = []
+// //         const [shuffledNewArray, setShuffledNewArray] = useState([]);
+
+
+// // const ShuffleData = () => {
+// //             let i = 0;
+                
+// //             while (i < myArray.length) {
+                
+// //             const randomIndex = Math.floor(Math.random() * myArray.length);
+                
+// //              if(!usedIndexes.includes(randomIndex)){
+// //                  shuffledArray.push(myArray[randomIndex])
+// //                  usedIndexes.push(randomIndex)
+// //                  i++
+// //              }   
+                        
+// //             }
+// //         setShuffledNewArray(shuffledArray)
+
+// //         }
+
+// // useEffect(()=>{
+// //         ShuffleData();
+// // },[])
+
+//   return (
+//     <>
+//       <SideBar>
+//       <div>
+//             {/* <button onClick={ShuffleData} style={{ margin: "200px" }}>
+//               Shuffle
+//             </button> */}
+//             {/* <p>{shuffledNewArray}</p> */}
+      
+//             {/* <ul>
+//               {shuffledNewArray.map((item, index) => (
+//                 <li key={index}>{item}</li>
+//               ))}
+//             </ul> */}
+
+
+//            {/* <Table dataSource={shuffledNewArray} columns={columns} />; */}
+
+
+//      hi
+
+
+//           </div>
+//       </SideBar>
+        
+//     </>
+//   )
+// }
+
+import React, { useEffect, useState } from 'react'
+import SideBar from '../DashboardSideBar/SideBar'
+import axios from 'axios'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Table, message } from 'antd'
+
+export default function Fixture() {
+
+        const [teamData , setTeamData] = useState([])
+        const shuffledArray = []
+        const usedIndexes = []
+        const [shuffledNewArray, setShuffledNewArray] = useState([]);
+        // const [newArrayLength,setNewArrayLength] = useState([])
+        const navigate = useNavigate()
+        const location = useLocation()
+        const matches = [];
+ 
+ const getTeamData = async()=>{
+        const response = await axios.get("http://localhost:8080/api/v1/fixture/get-team")
+        console.log(response.data.data);
+        setTeamData(response.data.data)
+        setShuffledNewArray(response.data.data)
+        // setNewArrayLength(response.data.data)
+        // console.log(newArrayLength.length);
+ }
+
+
+ const ShuffleData = () => {
+            let i = 0;
+            console.log(teamData);
+                
+            while (i < teamData.length) {
+                
+            const randomIndex = Math.floor(Math.random() * teamData.length)
+            // console.log(randomIndex);
+                
+             if(!usedIndexes.includes(randomIndex)){
+                 shuffledArray.push(teamData[randomIndex])
+                 usedIndexes.push(randomIndex)
+                 i++
+             }   
+
+            // console.log(shuffledArray);
+            // i++
+                        
+            }
+        setShuffledNewArray(shuffledArray)
+        console.log(shuffledArray);
+
+      
+      }
+
+      
+const shuffleDataStore = async()=>{
+        try {
+
+        console.log("shuffled data : " ,shuffledNewArray);
+        const response = await axios.post("http://localhost:8080/api/v1/shuffle/newTeam",shuffledNewArray)
+        // console.log(response.data.savedDocument._id);
+        const shuffledDataId = response.data.savedDocument._id
+        message.success(response.data.message)
+        navigate("/final-fixture",{state:{shuffledDataId:shuffledDataId}})
+
+        } catch (error) {
+          message.error("Shuffle fixture save have an error")
+        }      
+      }
+
+//  const handleSingleEliminate = async()=>{
+//   navigate("/test-fixture", { state: { teamsCount: newArrayLength.length } });
+//  }
+
+const handleDelete = async(id)=>{
+   try {
+    const response = await axios.post("http://localhost:8080/api/v1/delete/delte-team",{id:id})
+    message.success(response.data.message)
+    window.location.reload()
+    
+   } catch (error) {
+    message.error("Delete have an error")
+    
+   }
+}
+
+
+useEffect(()=>{
+        getTeamData()
+        ShuffleData();
+ },[])
+
+
+  return (
+    <>
+     <SideBar>
+         {/* <div>
+             {shuffledNewArray.map((data)=>(
+                <p>{data.TeamName}</p>
+             ))}
+         </div> */}
+
+
+    <div className="fixtureContainer">
+
+              <Table
+                className="Table"
+                columns={[
+                  {
+                    title: "Team Number",
+                    width: "10%",
+                    dataIndex: "teamNumber",
+                    render: (text, record, index) => (
+                      <span key={index}>{index + 1}</span>
+                    )
+                  },                  
+                  {
+                    title: "Teams Name",
+                    dataIndex: "teamName",
+                    render: (text, record) =>(
+                      <span>{record.TeamName}</span>
+                    )
+                  },
+
+                  {
+                    title: "Event Time",
+                    dataIndex: "time",
+                    render: (text, record) => <span>8.30am</span>,
+                  },
+
+                  {
+                    title: "Location",
+                    dataIndex: "location",
+                    render: (text, record) => <span>Ground 01</span>,
+                  },
+
+                  {
+                    title: "Action",
+                    dataIndex: "action",
+                    render: (text, record) => (
+                      <span><button onClick={()=>handleDelete(record._id)}>delete</button></span>
+                    ),
+                  },
+
+                ]}
+                pagination={{
+                  style: {
+                    marginTop: "50px",
+                  },
+                  pageSize: 100,
+                }}
+
+                // Displaying data from the backend
+                dataSource={shuffledNewArray}
+              >
+
+              </Table>
+
+        <button onClick={ShuffleData}>Suffle</button>
+        <button onClick={shuffleDataStore}>Save Shuffle</button>
+        {/* <button onClick={handleSingleEliminate}>Single Eliminate</button> */}
+
+
+      </div>
+
+     </SideBar>
+    </>
+  )
+}
+
