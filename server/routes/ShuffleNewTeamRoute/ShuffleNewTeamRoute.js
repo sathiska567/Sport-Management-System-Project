afd539e2579ab4a64d841581ce4409badb993cae
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-01-25 20:17:30 +0530

    updated

--- server/routes/ShuffleNewTeamRoute/ShuffleNewTeamRoute.js
+++ server/routes/ShuffleNewTeamRoute/ShuffleNewTeamRoute.js
@@ -1,13 +1,13 @@
 
-// const express = require('express');
-// const { shuffledNewTeamController,getShuffledNewTeamController } = require('../../controllers/ShuffleNewTeam/ShuffleNewTeamController');
+const express = require('express');
+const { shuffledNewTeamController,getShuffledNewTeamController } = require('../../controllers/ShuffleNewTeam/ShuffleNewTeamController');
 
-// const router = express.Router();
+const router = express.Router();
 
-// // SEND NEW SHUFFLED TEAM DATA || POST
-// router.post("/newTeam",shuffledNewTeamController)
+// SEND NEW SHUFFLED TEAM DATA || POST
+router.post("/newTeam",shuffledNewTeamController)
 
-// // GET SHUFFLED NEW TEAM || GET
-// router.post("/newFixture",getShuffledNewTeamController)
+// GET SHUFFLED NEW TEAM || GET
+router.post("/newFixture",getShuffledNewTeamController)
 
-// module.exports = router;
+module.exports = router;
