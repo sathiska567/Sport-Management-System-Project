cf2cc8ddcb4b94a28a95855a002b50b2251feb9a
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-01-25 20:13:24 +0530

    updated

---
+++ server/controllers/ShuffleNewTeam/ShuffleNewTeamController.js
@@ -0,0 +1,60 @@
+// const ShuffledNewTeamModel = require("../../models/");
+
+// const shuffledNewTeamController = async (req, res) => {
+//   try {
+//     const dataArray = req.body;
+//     console.log(dataArray);
+
+//     // Extract TeamName values from dataArray
+//     const teamNames = dataArray.map(item => item.TeamName);
+//     console.log(teamNames);
+
+//     // Create a new document using the ShuffledNewTeamModel
+//     const document = new ShuffledNewTeamModel({ newTeam: teamNames });
+//     console.log(document);
+
+//     // Save the document to the database
+//     const savedDocument = await document.save();
+
+//     // Respond with the saved document or an appropriate response
+//     res.status(200).send({
+//       success: true,
+//       message: "Shuffle updated successfully",
+//       savedDocument,
+//     });
+    
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).send({
+//       success: false,
+//       message: "Error updating shuffle",
+//     });
+//   }
+// };
+
+
+// // GET DATA || GET
+// const getShuffledNewTeamController = async(req,res)=>{
+
+//   try {
+//     const {id} = req.body;
+//     console.log("shuffle get id is " , id);
+//     const data = await ShuffledNewTeamModel.findById(id);
+//     console.log(data);
+//     res.status(200).send({
+//       success:true,
+//       message:"Data Fetched Successfully",
+//       data
+//     })
+    
+//   } catch (error) {
+//     res.status(400).send({
+//       success:false,
+//       message:"Data Fetched Unsuccessfully",
+//       error
+//     })
+//   }
+
+// }
+
+// module.exports = { shuffledNewTeamController,getShuffledNewTeamController };
---
+++ server/routes/ShuffleNewTeamRoute/ShuffleNewTeamRoute.js
@@ -0,0 +1,13 @@
+
+// const express = require('express');
+// const { shuffledNewTeamController,getShuffledNewTeamController } = require('../../controllers/ShuffleNewTeam/ShuffleNewTeamController');
+
+// const router = express.Router();
+
+// // SEND NEW SHUFFLED TEAM DATA || POST
+// router.post("/newTeam",shuffledNewTeamController)
+
+// // GET SHUFFLED NEW TEAM || GET
+// router.post("/newFixture",getShuffledNewTeamController)
+
+// module.exports = router;
