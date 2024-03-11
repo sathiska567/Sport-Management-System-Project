a42450426ef043e9331c75b1ef11435b3ad1b6c3
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-03-11 12:13:26 +0530

    apply position application
    apply position form updated

--- project/src/Components/ApplyPosition/ApplyPosition.jsx
+++ project/src/Components/ApplyPosition/ApplyPosition.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Form, Input, Row, message } from "antd";
 import SideBar from "../DashboardSideBar/SideBar";
 import axios from "axios";
@@ -20,6 +20,33 @@ const ApplyPosition = () => {
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
+
+//   // GET CURRENT USER DATA
+const currentUserData = async () => {
+  try {
+    const res = await axios.get(
+      "http://localhost:8080/api/v1/user/getCurrentUser",
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+  if(res.data.user.messages.length > 0){
+    // console.log(res.data.user.messages[0]);
+    message.success(res.data.user.messages[0])
+    // console.log(res.data.user);
+  }
+
+  console.log(res.data.user);
+  setNewEmail(res.data.user.email)
+  } catch (error) {
+    message.error("Error have inside the Get currentUserData function");
+  }
+};
+
+
 
   // handle position registration
   const handleSubmit = async (values) => {
@@ -46,6 +73,11 @@ const ApplyPosition = () => {
       message.error(error);
     }
   };
+
+
+useEffect(()=>{
+  currentUserData()
+},[])
 
   return (
     <div>
@@ -82,8 +114,10 @@ const ApplyPosition = () => {
                 id="FistName"
                 name="FirstName"
                 className="formInput"
-                onChange={(e) => setFirstName(e.target.value)}
+                onChange={(e) => setFirstName(e.target.value)}                
               />
+              <p style={{color:"red",fontSize:"12px"}}>{FirstName ? " " : "Please Enter Your First Name ! "}</p>
+
 
               <label htmlFor="">Last Name:</label>
               <input
@@ -93,6 +127,8 @@ const ApplyPosition = () => {
                 className="formInput"
                 onChange={(e) => setLastName(e.target.value)}
               />
+              <p style={{color:"red",fontSize:"12px"}}>{LastName ? " " : "Please Enter Your Last Name ! "}</p>
+
 
               <label htmlFor="">Email:</label>
               <input
@@ -100,8 +136,11 @@ const ApplyPosition = () => {
                 id="Email"
                 name="Email"
                 className="formInput"
-                onChange={(e) => setNewEmail(e.target.value)}
+                // onChange={(e) => setNewEmail(e.target.value)}
+                value={newEmail}
               />
+              <p style={{color:"red",fontSize:"12px"}}>{newEmail ? " " : "Please Enter Your Email ! "}</p>
+
 
               <label htmlFor="">Age:</label>
               <input
@@ -111,6 +150,8 @@ const ApplyPosition = () => {
                 className="formInput"
                 onChange={(e) => setNewAge(e.target.value)}
               />
+           <p style={{color:"red",fontSize:"12px"}}>{newAge ? " " : "Please Enter Your Email ! "}</p>
+
 
               <label htmlFor="">UserRole:</label>
               <input
@@ -120,6 +161,8 @@ const ApplyPosition = () => {
                 className="formInput"
                 onChange={(e) => setUserRole(e.target.value)}
               />
+        <p style={{color:"red",fontSize:"12px"}}>{userRole ? " " : "Please Enter Your Email ! "}</p>
+
 
               <label htmlFor="">Distric:</label>
               <input
@@ -129,6 +172,8 @@ const ApplyPosition = () => {
                 className="formInput"
                 onChange={(e) => setDistric(e.target.value)}
               />
+              <p style={{color:"red",fontSize:"12px"}}>{distric ? " " : "Please Enter Your Email ! "}</p>
+
 
               <label htmlFor="">Experience:</label>
               <textarea
@@ -141,6 +186,7 @@ const ApplyPosition = () => {
                 rows="10"
                 onChange={(e) => setExperience(e.target.value)}
               ></textarea>
+                <p style={{color:"red",fontSize:"12px"}}>{experience ? " " : "Please Enter Your Email ! "}</p>
 
               <div className="buttonSet">
                 <button className="approve userAppBTn">
