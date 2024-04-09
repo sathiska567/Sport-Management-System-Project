69b3e53b8914de3eb37249d89ae4230dd055f427
Chathura <cpriydasun@gmail.com>
2024-04-10 00:24:58 +0530

    Admin dashbaord grapghs back end connected

--- project/src/Components/StatCard/StatCards.jsx
+++ project/src/Components/StatCard/StatCards.jsx
@@ -8,11 +8,18 @@ import { Line, Bar, Doughnut } from "react-chartjs-2";
 import axios from "axios";
 import { Clock } from "@sujitsimon/react-flipclock";
 import Calendar from "react-calendar";
+import { message } from "antd";
 // Destructuring components from Ant Design's Layout
 const { Content } = Layout;
 
 // Navbar component
 const StatCards = () => {
+  const [allApplyingForm, setAllApplyingForm] = useState([]);
+  let count = 0;
+  const [pendingArray, setPendingArray] = useState([]);
+  const [ApproveArray, setApprove] = useState([]);
+  // const pendingArray = []
+
   // Formatter function for CountUp component
   const formatter = (value) => <CountUp end={value} separator="," />;
   useEffect(() => {
@@ -28,6 +35,34 @@ const StatCards = () => {
       });
   }, []);
 
+  const getAllApplyingUser = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/v1/admin/get-all-details"
+      );
+      console.log(response.data.allApplyingDetails.length);
+      setAllApplyingForm(response.data.allApplyingDetails);
+
+      const pendingRequests = response.data.allApplyingDetails.filter(
+        (item) => item.status === "pending"
+      );
+      setPendingArray(pendingRequests);
+
+      const ApproveRequests = response.data.allApplyingDetails.filter(
+        (item) => item.status === "Approve"
+      );
+      setApprove(ApproveRequests);
+
+      console.log(pendingArray);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getAllApplyingUser();
+  }, []);
+
   // JSX structure for the Navbar component
   return (
     <SideBar>
@@ -36,7 +71,7 @@ const StatCards = () => {
         <Layout>
           {/* Content section with statistics */}
           <div className="Stats flex-container">
-            <div className="dataCard flex-container1st " >
+            <div className="dataCard flex-container1st ">
               <div className="flex-item1st">
                 <h3>Date</h3>
                 <Calendar className="my-calendar" />
@@ -55,11 +90,21 @@ const StatCards = () => {
             <div className="dataCard PositionCard">
               <Bar
                 data={{
-                  labels: ["Requested", "Approved", "Pending", "Deleted"],
+                  labels: [
+                    "All Requests",
+                    "Pending Request",
+                    "Approve Request",
+                    "Deleted",
+                  ],
                   datasets: [
                     {
-                      label: "Number of Positions",
-                      data: [150, 100, 75, 50],
+                      label: "All Form Submition",
+                      data: [
+                        allApplyingForm.length,
+                        pendingArray.length,
+                        ApproveArray.length,
+                        50,
+                      ],
                       backgroundColor: ["red", "blue", "green", "orange"],
                     },
                   ],
