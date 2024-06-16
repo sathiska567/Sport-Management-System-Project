a40e4e4b8a3dd496a74caac12c4d71bdfc1d4afa
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-06-16 22:37:22 +0530

    player backend pagination done.

--- server/routes/GetAllPlayerDetails/getPlayerDetailsRoute.js
+++ server/routes/GetAllPlayerDetails/getPlayerDetailsRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getPlayerDetailsController } = require('../../controllers/GetAllPlayerController/getAllPlayerController');
+const { getPlayerDetailsController, playerPaginationController } = require('../../controllers/GetAllPlayerController/getAllPlayerController');
 
 const router = express.Router();
 
 // GET ONLY PLAYER DETAILS
 router.get("/player-details",getPlayerDetailsController)
+
+// Player PAGINATION || POAT
+router.post("/player-pagination",playerPaginationController)
 
 
 module.exports = router;
