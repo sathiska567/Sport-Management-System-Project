28e037dec48f41abfa7a6d4fcfdba69f4a95c46b
Chathura <cpriydasun@gmail.com>
2024-07-07 09:32:21 +0530

    revert "Merge pull request #228 from sathiska567/testing03"
    This reverts commit d71b0ad1170ffe31dcbc779e2f7cdc6caa615fbc, reversing
    changes made to 19103863250ae639548dad730ad7ba2202ddadaa.

--- server/controllers/PointTableFormController/PointTableFormController.js
+++ server/controllers/PointTableFormController/PointTableFormController.js
@@ -1,41 +1,80 @@
 const PointTableFormModel = require("../../models/PointTableFormModel/PointTableFormModel");
 
+
+
 const PointTableForm = async (req, res) => {
-  try {
-    const {
-      nameOfTheMatch,
-      nameOfTheTeam,
-      wonMatches,
-      lostMatches,
-      totalRunsEachTeamMatches,
-      totalOversEachTeam,
-      totalMarksForEachTeam,
-    } = req.body;
 
-    const data = new PointTableFormModel({
-      nameOfTheMatch,
-      nameOfTheTeam,
-      wonMatches,
-      lostMatches,
-      totalRunsEachTeamMatches,
-      totalOversEachTeam,
-      totalMarksForEachTeam,
-    });
 
-    await data.save();
+   try {
+      const {
+         nameOfTheMatch,
+         nameOfTheTeam,
+         wonMatches,
+         lostMatches,
+         totalRunsEachTeamMatches,
+         totalOversEachTeam,
+         totalMarksForEachTeam
+      } = req.body;
 
-    res.status(200).send({
-      success: true,
-      message: "Event Created Successfully",
-      data,
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      message: "Failed to create event details",
-      error,
-    });
-  }
-};
 
-module.exports = { PointTableForm };
+
+      console.log(
+         nameOfTheMatch,
+         nameOfTheTeam,
+         wonMatches,
+         lostMatches,
+         totalRunsEachTeamMatches,
+         totalOversEachTeam,
+         totalMarksForEachTeam
+      );
+
+
+
+      const data = new PointTableFormModel({
+         nameOfTheMatch,
+         nameOfTheTeam,
+         wonMatches,
+         lostMatches,
+         totalRunsEachTeamMatches,
+         totalOversEachTeam,
+         totalMarksForEachTeam
+      });
+
+      
+
+      await data.save();
+
+      res.status(200).send({
+         success: true,
+         message: "Event Created Successfully",
+         data
+      });
+   } catch (error) {
+       res.status(400).send({
+          success: false,
+          message: "Failed to create event details",
+          error
+       })
+   }
+}
+
+
+const GetPointTableFormController = async(req,res)=>{
+   try {
+      const allCreatedPointTableDetails = await PointTableFormModel.find({});
+      res.status(200).send({
+         success: true,
+         message: "All Created Point Table Details",
+         allCreatedPointTableDetails
+      })
+      
+   } catch (error) {
+      res.status(400).send({
+         success: false,
+         message: "Failed to create event details",
+         error
+      })
+   }
+}
+
+module.exports = { PointTableForm,GetPointTableFormController };
