0c6ef30940f1aeac2fa4383992b6688061daa52d
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-07-08 12:26:40 +0530

    modify

--- project/src/Components/Stats/CoachStats/teamOverview.js
+++ project/src/Components/Stats/CoachStats/teamOverview.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import { message } from 'antd';
+import axios from "axios";
 
-const teamOverview = ({ createdCount, rejectedCount }) => {
-  const series = [createdCount, rejectedCount];
+const TeamOverview = ({ createdCount, rejectedCount }) => {
+   const [availableAllCoach , setAvailableAllCoach] = useState([]);
+   const [allCoaches , setAllCoaches] = useState([]);
+
+  const series = [availableAllCoach.length, allCoaches.length];
+
+
+  const getAllCoaches = async()=>{
+    try {
+      const allPlayerResponse = await axios.get("http://localhost:8080/api/v1/coach/details")
+        // console.log(allPlayerResponse.data.data);
+        setAllCoaches(allPlayerResponse.data.data)
+    } catch (error) {
+      message.error('Something went wrong');
+    }
+  }
+
+
+  const getAllAvailableCoaches = async () => {
+    try {
+      const availableCoachResponse = await axios.get("http://localhost:8080/api/v1/availability/get-available-coach");
+      // console.log(availableCoachResponse.data.data);
+      setAvailableAllCoach(availableCoachResponse.data.data)
+    } catch (error) {
+      message.error('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    getAllAvailableCoaches();
+    getAllCoaches()
+  }, []);
 
   const options = {
     chart: {
@@ -33,11 +65,11 @@ const teamOverview = ({ createdCount, rejectedCount }) => {
         },
       },
     ],
-    labels: ["Created", "Cancelled"],
+    labels: ["Available", "All Coaches"],
     colors: ["#2f54eb", "#adc6ff"],
   };
 
-  return <ReactApexChart options={options} series={series} type="donut" createdCount={12} rejectedCount={5}/>;
+  return <ReactApexChart options={options} series={series} type="donut" />;
 };
 
-export default teamOverview;
+export default TeamOverview;
